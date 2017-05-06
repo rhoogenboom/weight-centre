7d9d4447d3a59fd14934e9f5bb0e9e31e459e7a9
Richard Hoogenboom <richard.hoogenboom@belleron.net>
2017-05-07 00:49:59 +0200

    working base version

---
+++ weight-main/settings.h
@@ -0,0 +1,14 @@
+// SERIAL
+// ------------------------------------------------------------------------------------------------------------------------------------------------>
+    #define BaudRate                 38400          // Set baud rate here if you know what you're doing and don't like the default value
+
+
+
+// DEBUGGING
+// ------------------------------------------------------------------------------------------------------------------------------------------------>
+    #define DEBUG                     false         // Set this to true to receive debugging messages out the serial port. NOTE: This will make the program less responsive, 
+                                                    // so turn it off when you are done troubleshooting. 
+
+
+
+
