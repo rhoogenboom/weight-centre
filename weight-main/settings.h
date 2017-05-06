a65ce1c8612715649748713eb628badc1112d435
Richard Hoogenboom <richard.hoogenboom@belleron.net>
2017-05-07 00:53:36 +0200

    removed SimpleTimer from local folder, moved to library

--- weight-main/settings.h
+++ weight-main/settings.h
@@ -9,6 +9,9 @@
     #define DEBUG                     false         // Set this to true to receive debugging messages out the serial port. NOTE: This will make the program less responsive, 
                                                     // so turn it off when you are done troubleshooting. 
 
+// STREET PARTS
+// ----------------------------
+
 
 
 
